548a5186850d1a68cbef55a6cfffcf9299a0af95
Tim <tim.leon.persson@gmail.com>
2024-04-13 17:39:12 +0200

    update

--- src/lib.rs
+++ src/lib.rs
@@ -21,7 +21,7 @@ pub mod share_link;
 pub mod unix_timestamp;
 pub mod util;
 
-#[derive(Clone, Eq, PartialEq)]
+#[derive(Clone, Eq, PartialEq, strum::Display)]
 pub enum WebhookSignatureScheme {
     ED25519,
     HMAC_SHA256,
